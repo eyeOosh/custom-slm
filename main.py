110bff945c5a46e34bc7e08adcf904d9f5fc1018
eyeOosh <aayushkumar1824@gmail.com>
2026-04-26 02:01:41 -0700

    day 2

--- main.py
+++ main.py
@@ -6,6 +6,11 @@
 
 import re
 
+print("\nLearning LLMs from Scratch - Aayush Kumar")
+
+print(f"="*50)
+print("Part 1: Load in Text")
+print(f"="*50, "")
 # 1. load in text
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
@@ -13,12 +18,18 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
 print(len(raw_text))
 print(raw_text[:99])
 
+print("\n" + f"="*50)
+print("Part 2: Tokenizing")
+print(f"="*50, "")
 # 2. Tokenization time
 preproc = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
 preproc = [item.strip() for item in preproc if item.strip()]
 print(preproc[:30])
 print("Length of text:", len(preproc))
 
+print("\n" + f"="*50)
+print("Part 3: Encoding")
+print(f"="*50, "")
 # 3. Convert tokens into token IDs - Encoding
 # vocabulary is a dictionary of unique tokens sorted alphabetically
 # then we assign token IDs to the vocab
@@ -81,6 +92,10 @@ tokenizer2 = Tokenizer2(vocabulary)
 text1 = "Hello, I'm a human"
 text2 = "In the midsummer solstice sulit terraces"
 
+print("\n" + f"="*50)
+print("Part 3.1: Encoding with <|unk|> and <|endoftext|>")
+print(f"="*50, "")
+
 text = " <|endoftext|> ".join((text1, text2))
 ids2 = tokenizer2.encode(text)
 print("IDs (2):", ids2)
@@ -109,3 +124,68 @@ print(tokenizer2.decode(ids2))
 # so make 'est</w>' a new token, then find that 'o' and 'l' appear the most
 # so make 'ol' a new token, then find that 'ol' and 'd' appear the most
 # so make 'old' a new token, and remove all tokens with 0 apperances
+# use tiktoken
+import tiktoken
+import importlib
+tiktokenizer = tiktoken.get_encoding("gpt2")
+
+print("\n" + f"="*50)
+print("Part 3.2: Encoding with GPT-2 BPE using TikToken")
+print(f"="*50, "")
+
+text = "Hello matey! <|endoftext|> Waddup matey! What a great day, innit?"
+idsTT = tiktokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(idsTT)
+stringsTT = tiktokenizer.decode(idsTT)
+print(stringsTT)
+
+# note that this doesn't use <|unk|> token because its breaking down words to subwords and characters
+# encoding some nonsense like 'Akwirw ier' is still broken down correctly
+# like maybe 'ir' and 'er' are commonly occuring, which get replaced, and everything else as well
+# 50,256 total tokens in gpt2 and gpt3 BPE are encompassing everything in the 170k-250k words in english
+
+
+# 4. Input Target Pairs
+encodedText = tiktokenizer.encode(raw_text)
+print("Number of tokens in the raw text (aka Vocabulary size):", len(encodedText))
+encSample = encodedText[50:] # remove first 50 tokens from the data set
+
+contextSize = 4 #length of input
+# model is trained to look at 4 tokens to predict the next, 5th token
+print("\n" + f"="*50)
+print(f"Part 4: Input Target Pairs with a context size {contextSize}")
+print(f"="*50, "")
+
+x = encSample[:contextSize]
+y = encSample[1:contextSize + 1]
+
+print(f"x: {x}")
+print(f"y:      {y}")
+#x: [290, 4950, 2241, 287]
+#y:      [4950, 2241, 287, 257]
+# 290 --> 4950
+# 290, 4950 --> 2241
+# 290, 4950, 2241 --> 287
+# 290, 4950, 2241, 287 --> 257
+
+for i in range(1, contextSize + 1):
+    context = encSample[:i]
+    desired = encSample[i]
+    print("Input:", context, "  --> ", "Target:", desired)
+
+# now use dataset and dataloaders to make these arrays into tensors for PyTorch
+# sample text: 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+# x = tensor([[a, b, c, d],
+#             [e, f, g, h],
+#             [    ...   ]])
+# y = tensor([[b, c, d, e],
+#             [f, g, h, i],
+#             [    ...   ]])
+
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDataset(Dataset):
+    def __init__(self, txt, tokenizer, maxLen, stride):
+        self.inputIds = []
+        self.targetIds = []
+        
